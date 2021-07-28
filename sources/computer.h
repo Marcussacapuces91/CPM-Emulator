fbd1b6c629ea14b934b4a8fd6c8d704fb6ec7b0b
Marc Sibert <Marcussacapuces91@users.noreply.github.com>
2021-07-28 12:43:56 +0200

    Update computer.h

--- sources/computer.h
+++ sources/computer.h
@@ -1090,7 +1090,7 @@ protected:
  */
 	void warmBoot(ZZ80State& state) {
 		load("CCP-DR.64K", 0xF400);
-		state.Z_Z80_STATE_MEMBER_PC = 0xF400S;
+		state.Z_Z80_STATE_MEMBER_PC = 0xF400;
 	}
 	
 private:
