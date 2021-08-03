8fb28a4a099d233e90abd7578dd6fb12bca956c8
Marc Sibert <marc@sibert.fr>
2021-08-04 00:09:58 +0200

    Update bios.h

--- sources/bios.h
+++ sources/bios.h
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#include "Z80.h"
+// #include "Z80.h"
 
 // #include <dirent.h>
 // #include <sys/stat.h>
@@ -26,10 +26,35 @@
 // #include <filesystem>
 
 // #define LOG 1
-
+/**
+ * @see https://www.seasip.info/Cpm/bios.html#const
+ * 	JMP	BOOT	;-3: Cold start routine
+	JMP	WBOOT	; 0: Warm boot - reload command processor
+	JMP	CONST	; 3: Console status
+	JMP	CONIN	; 6: Console input
+	JMP	CONOUT	; 9: Console output
+	JMP	LIST	;12: Printer output
+	JMP	PUNCH	;15: Paper tape punch output
+	JMP	READER	;18: Paper tape reader input
+	JMP	HOME	;21: Move disc head to track 0
+	JMP	SELDSK	;24: Select disc drive
+	JMP	SETTRK	;27: Set track number
+	JMP	SETSEC	;30: Set sector number
+	JMP	SETDMA	;33: Set DMA address
+	JMP	READ	;36: Read a sector
+	JMP	WRITE	;39: Write a sector
+	JMP	LISTST	;42: Status of list device
+	JMP	SECTRAN	;45: Sector translation for skewing
+ */
 template <unsigned MEMORY_SIZE, uint16_t BIOS_ADDR>
 class BIOS {
 public:
+	BIOS() {
+		std::cout << "CP/M 2.2 Emulator " << MEMORY_SIZE << "kb" << std::endl;
+		std::cout << "Copyright (c) 2021 by M. Sibert" << std::endl;
+		std::cout << std::endl;
+	}
+	
 	void init(uint8_t *const memory) {
 	// WBOOT (BIOS entry)
 		memory[0x0000] = 0xC3;						// JUMP TO BIOS
@@ -41,10 +66,6 @@ public:
 			memory[BIOS_ADDR + (i * 3) + 1] = (BIOS_ADDR + (i * 3)) & 0xFF;
 			memory[BIOS_ADDR + (i * 3) + 2] = (BIOS_ADDR + (i * 3)) >> 8;
 		}
-
-		std::cout << "CP/M 2.2 Emulator " << MEMORY_SIZE << "kb" << std::endl;
-		std::cout << "Copyright (c) 2021 by M. Sibert" << std::endl;
-		std::cout << std::endl;
 	}
 
 /**
@@ -93,6 +114,6 @@ private:
 		CONST_ADDR 	= BIOS_ADDR + 3 * 2,
 		CONIN_ADDR 	= BIOS_ADDR + 3 * 3,
 		CONOUT_ADDR = BIOS_ADDR + 3 * 4,
-		LIST_ADDR 	= BIOS_ADDR + 3 * 5
+//		LIST_ADDR 	= BIOS_ADDR + 3 * 5
 	};
 };
